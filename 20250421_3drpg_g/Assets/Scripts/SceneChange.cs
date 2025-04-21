c4cf5840a6a453de090cd678a44d9d9cac649353
candy-hsu <62374659+candy-hsu@users.noreply.github.com>
2025-04-22 00:58:23 +0800

    20250422

--- 20250421_3drpg_g/Assets/Scripts/SceneChange.cs
+++ 20250421_3drpg_g/Assets/Scripts/SceneChange.cs
@@ -1,16 +1,11 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void sceneChange()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("Zone01");
     }
 }
